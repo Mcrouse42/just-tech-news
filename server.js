286062776c8e8dedb10538676e9e1f645b32ceb1
Mcrouse <matthew.crouse@temple.edu>
2021-02-11 15:26:35 -0500

    updated partials, date and comment formatting

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -18,23 +19,22 @@ const sess = {
   })
 };
 
-
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
 
-// turn on routes
-app.use(routes);
+const hbs = exphbs.create({ helpers });
 
-// turn on connection to database and server
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
